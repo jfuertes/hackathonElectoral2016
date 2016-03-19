42a5d6555869756c0498edbf3dda5e7831dc2d6f
Meka <mekatroniko@gmail.com>
2016-03-18 23:08:20 -0500

    Estructura basica con el esqueleto de angularJS cargado

---
+++ app/app.js
@@ -0,0 +1,8 @@
+(function(){
+  angular.module('jneApp', [ 'ngRoute',  'Controllers'])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+      when('/', { templateUrl: 'views/home.html', caseInsensitiveMatch: true, controller: 'HomeController' }).
+      otherwise({ redirectTo: '/' });
+    }]);
+})();
---
+++ app/controllers.js
@@ -0,0 +1,4 @@
+(function(){
+  angular.module('Controllers', [])
+  .controller('HomeController',['$scope', function ($scope) {   }]);
+})();
